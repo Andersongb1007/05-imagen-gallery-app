d133e24a07316eb3d02d386caf06afd2f878a4ca
Anderson Gonzalez <Anderson Gonzalez>
2023-02-20 11:23:33 -0400

    Inicio

---
+++ src/components/container.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const container = () => {
+    return (
+        <div className='container border'>
+
+            <div className="card" style={{ width: '18rem' }}>
+                <img className="card-img-top" src="https://" alt="profile" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text</p>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default container
