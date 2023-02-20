e3c26844319ad7c1657d70875f902bf3994fbca9
Anderson Gonzalez <andersongb1007@gmail.com>
2023-02-20 11:28:05 -0400

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from "./components/container"
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Galery app</h1>
+      <hr />
+      <Container />
+    </div>
+  )
+}
